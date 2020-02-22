01f2421470d27a24043ad484f14957b4d9cfc4d8
anqurvanillapy <anqurvanillapy@gmail.com>
2020-02-22 11:44:16 +0800

    [ cli, fixed #1 ] Use picocli for command line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,66 @@
-public class Main {
-    private static final String VERSION = "0.1.0";
+import java.io.File;
+import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Spec;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "aqira", version = "Aqira 0.1.0", description = "Aqira",
+        mixinStandardHelpOptions = true, subcommands = {
+        CmdBuild.class,
+        CmdRun.class,
+        CmdFormat.class,
+})
+public class Main implements Callable<Integer> {
+    @Spec
+    CommandSpec spec;
+
+    @Override
+    public Integer call() {
+        spec.commandLine().usage(System.out);
+        return 1;
+    }
 
     public static void main(String[] args) {
-        Cli cliArgs = Cli.parse(args);
+        int exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
+    }
+}
 
-        if (cliArgs == null) {
-            Cli.printHelp();
-            System.exit(1);
-        }
+@Command(name = "build", aliases = "b", description = "Build a source file.")
+class CmdBuild implements Callable<Integer> {
+    @Parameters(index = "0")
+    private File file;
 
-        switch (cliArgs.getSubCommand()) {
-            case "help":
-                Cli.printHelp();
-                break;
+    @Override
+    public Integer call() {
+        System.out.println("Building file: " + file);
+        return 0;
+    }
+}
 
-            case "version":
-                System.out.println("aqira " + VERSION);
-                break;
+@Command(name = "run", aliases = "r", description = "Run a source file.")
+class CmdRun implements Callable<Integer> {
+    @Parameters(index = "0")
+    private File file;
 
-            case "format":
-                System.out.println("formatting source file: " + cliArgs.getFile());
-                break;
+    @Override
+    public Integer call() {
+        System.out.println("Running file: " + file);
+        return 0;
+    }
+}
 
-            case "build":
-                System.out.println("building source file: " +
-                        cliArgs.getFile());
-                break;
+@Command(name = "fmt", aliases = "f", description = "Format a source file.")
+class CmdFormat implements Callable<Integer> {
+    @Parameters(index = "0")
+    private File file;
 
-            case "run":
-                System.out.println("running source file: " + cliArgs.getFile());
-                break;
-        }
+    @Override
+    public Integer call() {
+        System.out.println("Formatting file: " + file);
+        return 0;
     }
 }
